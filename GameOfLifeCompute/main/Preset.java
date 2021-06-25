7152bd440dba3b6aa87954f6454018c73f517d50
JosephParticles <71268741+JosephParticles@users.noreply.github.com>
2021-06-25 09:23:33 +0100

    Added presets and fixed directories being wrong

---
+++ GameOfLifeCompute/main/Preset.java
@@ -0,0 +1,31 @@
+package GameOfLifeCompute.main;
+
+public class Preset {
+    private int width;
+    private int height;
+
+    private float[] data;
+
+    public Preset(int width, int height, float[] data){
+        this.width=width;
+        this.height=height;
+
+        this.data=data;
+    }
+
+    public float[] getData(){
+        return data;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+
+    public int[] getSize(){
+        return new int[]{width,height};
+    }
+}
